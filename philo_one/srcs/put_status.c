41bfd6bb5fca30b00fd1b821f831bc04a302a1da
Alessio Glorioso <aglorios@mi-r1-p1.s19.be>
2021-02-24 10:45:07 +0100

    lockchange

--- philo_one/srcs/put_status.c
+++ philo_one/srcs/put_status.c
@@ -6,7 +6,7 @@
 /*   By: aglorios <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 15:42:11 by aglorios          #+#    #+#             */
-/*   Updated: 2019/10/22 16:43:58 by aglorios         ###   ########.fr       */
+/*   Updated: 2021/02/24 10:40:31 by aglorios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,6 @@ void	ft_put_status(t_data *data, char *philo, char *put, int i)
 	nbr = NULL;
 	str = NULL;
 	one = global_struct();
-	pthread_mutex_lock(&one->write);
 	gettimeofday(&data->end, NULL);
 	data->time = (data->end.tv_sec * 1000 + data->end.tv_usec / 1000) -
 					(one->start.tv_sec * 1000 + one->start.tv_usec / 1000);
@@ -66,6 +65,7 @@ void	ft_put_status(t_data *data, char *philo, char *put, int i)
 	else
 		str = ft_strjoin(str, put);
 	str = ft_strjoin(str, "\n");
+	pthread_mutex_lock(&one->write);
 	write(1, str, ft_strlen(str));
 	pthread_mutex_unlock(&one->write);
 }
