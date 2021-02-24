e1ea87c6381ea4f090e5b265e259ba2dea9eeb53
Alessio Glorioso <aglorios@shi-r1-p4.s19.be>
2021-02-24 15:34:33 +0100

    bug

--- philo_one/srcs/things.c
+++ philo_one/srcs/things.c
@@ -6,7 +6,7 @@
 /*   By: aglorios <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 15:42:11 by aglorios          #+#    #+#             */
-/*   Updated: 2019/10/22 16:43:58 by aglorios         ###   ########.fr       */
+/*   Updated: 2021/02/24 15:31:15 by aglorios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	*eating(void *arg, t_data *data)
 
 	one = global_struct();
 	ft_put_status(data, (char *)arg, "EAT", -1);
-	my_sleep(one->t_to_eat);
 	pthread_mutex_lock(&data->timing);
-	data->live += one->t_to_die;
+	my_sleep(one->t_to_eat);
+	data->live = get_time() + one->t_to_die;
 	pthread_mutex_unlock(&data->timing);
 	return (NULL);
 }
