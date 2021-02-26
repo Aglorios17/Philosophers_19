d873ab490882c32fd139155d20578245a9146477
Alessio Glorioso <aglorios@mi-r1-p1.s19.be>
2021-02-26 15:20:37 +0100

    write

--- philo_one/srcs/put_status.c
+++ philo_one/srcs/put_status.c
@@ -6,82 +6,55 @@
 /*   By: aglorios <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 15:42:11 by aglorios          #+#    #+#             */
-/*   Updated: 2021/02/25 15:43:08 by aglorios         ###   ########.fr       */
+/*   Updated: 2021/02/26 15:20:04 by aglorios         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo_one.h"
 
-char	*time_status(t_data *data, char *str, char *philo)
+void	time_status(t_data *data, char *philo)
 {
 	t_one	*one;
 	char	*fri;
 
-	fri = NULL;
 	one = global_struct();
+	fri = NULL;
 	gettimeofday(&data->end, NULL);
 	data->time = (data->end.tv_sec * 1000 + data->end.tv_usec / 1000) -
 					(one->start.tv_sec * 1000 + one->start.tv_usec / 1000);
 	fri = ft_itoa(data->time);
-	str = ft_strjoin(fri, " Philosopher ");
+	write(1, fri, ft_strlen(fri));
 	free(fri);
-	fri = str;
-	str = ft_strjoin(str, philo);
-	free(fri);
-	fri = str;
-	str = ft_strjoin(str, " ");
-	free(fri);
-	return (str);
+	write(1, " Philosopher ", 13);
+	write(1, philo, ft_strlen(philo));
+	write(1, " ", 1);
 }
 
-char	*fork_put(char *str, int i)
+void	fork_put(int i)
 {
 	char	*nbr;
-	char	*fri;
 
 	nbr = NULL;
-	fri = NULL;
-	fri = str;
-	str = ft_strjoin(str, "has fork ");
-	free(fri);
+	write(1, "has fork ", 9);
 	nbr = ft_itoa(i);
-	fri = str;
-	str = ft_strjoin(str, nbr);
-	free(fri);
+	write(1, nbr, ft_strlen(nbr));
 	free(nbr);
-	return (str);
 }
 
-char	*choose_status(char *str, char *put, int i)
+void	choose_status(char *put, int i)
 {
-	char *fri;
-
-	fri = NULL;
 	if (i != -1 && i != -2)
-		str = fork_put(str, i);
+		fork_put(i);
 	else if (i == -1)
-	{
-		fri = str;
-		str = ft_strjoin(str, put);
-		free(fri);
-	}
+		write(1, put, ft_strlen(put));
 	else
-	{
-		fri = str;
-		str = ft_strjoin(str, "est mort");
-		free(fri);
-	}
-	return (str);
+		write(1, "est mort", 8);
 }
 
 void	ft_put_status(t_data *data, char *philo, char *put, int i)
 {
-	char	*str;
-	char	*fri;
 	t_one	*one;
 
-	str = NULL;
-	fri = NULL;
 	one = global_struct();
 	pthread_mutex_lock(&one->write);
 	if (one->death == 1)
@@ -89,11 +62,8 @@ void	ft_put_status(t_data *data, char *philo, char *put, int i)
 		pthread_mutex_unlock(&one->write);
 		return ;
 	}
-	str = time_status(data, str, philo);
-	str = choose_status(str, put, i);
-	fri = str;
-	str = ft_strjoin(str, "\n");
-	free(fri);
-	write(1, str, ft_strlen(str));
+	time_status(data, philo);
+	choose_status(put, i);
+	write(1, "\n", 1);
 	pthread_mutex_unlock(&one->write);
 }
